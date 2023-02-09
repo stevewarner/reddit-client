a904ef07e4bd920051b61a87f3fdf34b08296aa9
Steven Warner <stevenmichaelwarner@gmail.com>
2023-02-09 00:31:02 -0800

    fix scaling

--- app/about/head.tsx
+++ app/about/head.tsx
@@ -2,6 +2,9 @@ export default function Head() {
   return (
     <>
       <title>About</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="Progressive Web App Reddit Client" />
+      <link rel="icon" href="/favicon.ico" />
     </>
   );
 }
