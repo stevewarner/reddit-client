32c91289c5e0b61ad37e0442312f50a27f6849ef
Steve <stevenmichaelwarner@gmail.com>
2025-08-08 21:25:47 -0700

    Update api.ts

--- utils/api.ts
+++ utils/api.ts
@@ -8,7 +8,7 @@ type Props = {
 export const getPosts = async ({ sub, after = null }: Props) => {
   const res = await fetch(
     `https://www.reddit.com/${sub}.json?after=${after}&raw_json=1&sr_detail=true`,
-    { cache: 'no-store' }
+    { next: { revalidate: 60 } }
   );
 
   if (!res.ok) {
